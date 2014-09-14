2112d016d41281ce4bc8f6daf7103c2572d8fa55
Yoshio Teruia <yteruia@gmail.com>
2014-09-14 06:56:43 +0000

    Add validation for creditcard number

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -12,4 +12,22 @@ class CreditCard
   def number
     @number.to_s.gsub(/\s/, '').to_i
   end
+
+  def valid?
+    numbers = @number.to_s.reverse.split('').map.with_index do |n, i|
+      i += 1
+      n = n.to_i
+      if i.even?
+        sum = n + n
+        if sum.to_s.length > 1
+          sum.to_s.split('').map(&:to_i).inject(:+).to_i
+        else
+          sum
+        end
+      else
+        n
+      end
+    end
+    numbers.inject(:+) % 10 == 0
+  end
 end
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -2,6 +2,52 @@ require 'rspec'
 require_relative '../lib/credit_card'
 
 describe CreditCard do
+  describe '#valid?' do
+    context 'valid number' do
+      it 'returns true' do
+        credit_card_number = 4111111111111111
+
+        credit_card = CreditCard.new(credit_card_number)
+
+        expect(credit_card.valid?).to be
+      end
+
+      it 'returns true' do
+        credit_card_number = 4012888888881881
+
+        credit_card = CreditCard.new(credit_card_number)
+
+        expect(credit_card.valid?).to be
+      end
+
+      it 'returns true' do
+        credit_card_number = 5105105105105100
+
+        credit_card = CreditCard.new(credit_card_number)
+
+        expect(credit_card.valid?).to be
+      end
+    end
+
+    context 'invalid number' do
+      it 'returns false' do
+        credit_card_number = 4111111111111
+
+        credit_card = CreditCard.new(credit_card_number)
+
+        expect(credit_card.valid?).not_to be
+      end
+
+      it 'returns false' do
+        credit_card_number = 4105105105105100
+
+        credit_card = CreditCard.new(credit_card_number)
+
+        expect(credit_card.valid?).not_to be
+      end
+    end
+  end
+
   describe '#number' do
     context 'numbers without spaces' do
       it 'returns the number' do
